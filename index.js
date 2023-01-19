b94979e812c3505eb24271bfe56a5bb9042309db
Oskar Krolik <oskar.krolik@microsoft.wsei.edu.pl>
2023-01-19 04:42:37 +0100

    Dodawanie taskow - I

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let todoInput; //miejsce gdzie wpisuje uzytkownik
 let errorInfo; //info o braku zadan / koniecznosci wpisania tekstu
 let addBtn; //przycisk ADD - dodaje nowe elementy do listy
 let ulList; //lista zadań, tagi ul
+let newTodo; //nowo dodany task
 
 const main = () => {
 	prepareDOMElements();
@@ -18,6 +19,28 @@ const prepareDOMElements = () => {
 
 const prepareDOMEvents = () => {
 	//tutaj nadaje nasluchiwanie
+	addBtn.addEventListener('click', addNewTodo);
+};
+
+/* 
+    funkcja dodajaca zadania:
+    1.przechwytuje tresc
+    2.tworzy nowy elemnt li
+    3.dodaje nowy element do ul
+    4.odpala sie na clicka ADD
+    5.nie doda pustego zadania
+*/
+const addNewTodo = () => {
+	if (todoInput.value !== '') {
+		newTodo = document.createElement('li');
+		newTodo.textContent = todoInput.value;
+		ulList.append(newTodo);
+
+		todoInput.value = '';
+		errorInfo.textContent = '';
+	} else {
+		errorInfo.textContent = 'Wpisz treść zadania!';
+	}
 };
 
 document.addEventListener('DOMContentLoaded', main);
